c6bc74332a9dfac56cc9f3676b717ed3ecf37a26
Xu Shaohua <shaohua@biofan.org>
2021-04-21 16:59:28 +0800

    xz: Support multiple threads

--- src/base/compress.rs
+++ src/base/compress.rs
@@ -6,6 +6,7 @@ use flate2::{Compression, GzBuilder};
 use std::fs::File;
 use std::io;
 use std::path::Path;
+use xz2::stream::MtStreamBuilder;
 use xz2::write::XzEncoder;
 
 use crate::BuildError;
@@ -23,8 +24,14 @@ pub fn create_gz(in_path: &Path, out_path: &Path) -> Result<(), BuildError> {
 
 pub fn create_xz2(in_path: &Path, out_path: &Path) -> Result<(), BuildError> {
     let out_file = File::create(out_path)?;
-    let xz_level = 9;
-    let mut xz = XzEncoder::new(out_file, xz_level);
+
+    let xz_level = 6;
+    let mut stream = MtStreamBuilder::new()
+        .preset(xz_level)
+        .threads(num_cpus::get() as u32)
+        .encoder()?;
+
+    let mut xz = XzEncoder::new_stream(out_file, stream);
 
     let mut in_file = File::open(in_path)?;
     io::copy(&mut in_file, &mut xz)?;
--- src/error.rs
+++ src/error.rs
@@ -5,6 +5,14 @@ pub enum BuildError {
     IoError(io::Error),
 
     FilesNotSet,
+
+    Lz2EncodeError,
+}
+
+impl From<xz2::stream::Error> for BuildError {
+    fn from(_err: xz2::stream::Error) -> Self {
+        BuildError::Lz2EncodeError
+    }
 }
 
 impl From<io::Error> for BuildError {
