36cc8489681dd8b2c9c6b7b360610e281a5f0708
Xu Shaohua <shaohua@biofan.org>
2021-08-23 11:56:17 +0800

    Add download list config

--- src/download.rs
+++ src/download.rs
@@ -2,8 +2,21 @@
 // Use of this source is governed by General Public License that can be found
 // in the LICENSE file.
 
+use serde_derive::{Deserialize, Serialize};
+
+use crate::base::config::Arch;
 use crate::Error;
+
+#[derive(Debug, Deserialize, Serialize)]
+struct DownloadEntry {
+    arch: Arch,
+    url: String,
+    filename: String,
+    sha256: String,
+}
 
 pub fn download() -> Result<(), Error> {
     Ok(())
 }
+
+fn download_file() {}
